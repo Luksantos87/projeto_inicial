3019106992f5093e41acb087e298886ceabbd882
Lucas Santos <luksantos87@gmail.com>
2024-06-12 22:59:49 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jorndada de dados")
+
+# Variáveis
+Idade = 34
+Nome = "Lucas Santos"
+
+# Print
+print(Idade)
+
+# Soma
+Nova_idade = Idade + 1
+print(Nova_idade)
+
+Lista = [1,2,3,4,5]
+
+# Métodos
+Lista_invertida = Lista.reverse()
+
+
+
+
